55610aa752ecac74f2c4c39334db0fb3aaaec42c
xianeng <945153535@qq.com>
2022-08-02 18:20:25 +0800

    fix: 借币图表展示14天的数据

--- src/main/java/com/tianli/borrow/service/impl/BorrowCoinOrderServiceImpl.java
+++ src/main/java/com/tianli/borrow/service/impl/BorrowCoinOrderServiceImpl.java
@@ -751,7 +751,7 @@ public class BorrowCoinOrderServiceImpl extends ServiceImpl<BorrowCoinOrderMappe
         DateTime beginOfDay = DateUtil.beginOfDay(DateUtil.offsetDay(new Date(), offsetDay));
         //构建十四天的数据
         Map<String,BorrowOrderStatisticsChartVO> borrowOrderStatisticsChartVOMap = new LinkedHashMap<>();
-        for(int i = offsetDay;i<=0;i++){
+        for(int i = offsetDay; i < 0; i++){
             String dateTime = DateUtil.format(DateUtil.offsetDay(new Date(), i), "yyyy-MM-dd");
             borrowOrderStatisticsChartVOMap.put(dateTime,new BorrowOrderStatisticsChartVO(dateTime,BigDecimal.ZERO));
         }
