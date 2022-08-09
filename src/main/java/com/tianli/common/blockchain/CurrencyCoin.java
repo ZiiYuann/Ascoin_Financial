b6825ddcf6fb19e4e03d6fa7575bd126c705118a
ziyuan <1149788726@qq.com>
2022-08-09 14:59:05 +0800

    [feat] 修改币别logo地址

--- src/main/java/com/tianli/common/blockchain/CurrencyCoin.java
+++ src/main/java/com/tianli/common/blockchain/CurrencyCoin.java
@@ -9,10 +9,10 @@ import java.util.List;
 // 各种币ø
 @Getter
 public enum CurrencyCoin {
-    usdt("https://financial-dev-jp.s3-accelerate.amazonaws.com/file/66e1ea07-7da8-459b-8d50-f49f1e36cd5e.png"),
-    usdc("https://financial-dev-jp.s3-accelerate.amazonaws.com/file/0887ce3c-4699-4d40-8992-5acb5e5b760c.png"),
-    bnb("https://financial-dev-jp.s3-accelerate.amazonaws.com/file/eb26dd33-9638-45eb-9501-6ec5771954eb.png"),
-    eth("https://financial-dev-jp.s3-accelerate.amazonaws.com/file/31cf3184-ed54-4872-803a-092a894b7ad8.png");
+    usdt("https://twallet-news-jp.oss-accelerate.aliyuncs.com/file/06e76726-793c-413e-b8c0-362d38018567.png"),
+    usdc("https://twallet-news-jp.oss-accelerate.aliyuncs.com/file/f01d6cd4-d0d5-4330-aeba-f82e1db54c2e.png"),
+    bnb("https://twallet-news-jp.oss-accelerate.aliyuncs.com/file/37706386-5421-434e-a2bf-5cb21d4255da.png"),
+    eth("https://twallet-news-jp.oss-accelerate.aliyuncs.com/file/bce61785-ea06-4edd-a7a2-fa7ccf35468b.png");
 
     CurrencyCoin(String logoPath) {
         this.name = super.name();
