d44dc2271e09e1c0079853cf4eced503c887ff78
ziyuan <1149788726@qq.com>
2022-08-31 16:47:18 +0800

    重启

--- src/test/java/com/tianli/chain/service/contract/TronTriggerContractTest.java
+++ src/test/java/com/tianli/chain/service/contract/TronTriggerContractTest.java
@@ -17,10 +17,10 @@ class TronTriggerContractTest {
 
     @Resource
     private BscTriggerContract bscTriggerContract;
+
     @Test
     void getStatusByHash() {
-//        tronTriggerContract.getStatusByHash("a4d9e15b137a1b1535cc04743861110c941395c7439859fcdda84592732bcd9d");
-        bscTriggerContract.successByHash("0x265d355523f160668a18adeeb52637f9675bebfa96946260c145da8450c49206");
+        tronTriggerContract.successByHash("00d704b30379b7137fd0b5a96531a8eea7e077b68a39513e026c006083bd5cfd");
     }
 
 }
