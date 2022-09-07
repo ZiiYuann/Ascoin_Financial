ca105e08a0fdba3b268fcc47147eb10204c7909e
xianeng <945153535@qq.com>
2022-09-07 16:34:42 +0800

    feat: 基金理财相关

--- src/main/java/com/tianli/task/FundIncomeTask.java
+++ src/main/java/com/tianli/task/FundIncomeTask.java
@@ -12,6 +12,7 @@ import com.tianli.fund.service.IFundIncomeRecordService;
 import com.tianli.fund.service.IFundRecordService;
 import lombok.extern.log4j.Log4j2;
 import org.redisson.api.RAtomicLong;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -46,51 +47,60 @@ public class FundIncomeTask {
         String day = String.format("%s_%s", now.getMonthValue(), now.getDayOfMonth());
         String redisKey = RedisLockConstants.FUND_INCOME_TASK + day;
         long page = incr(redisKey,60L);
-
         List<FundRecord> records = fundRecordService.page(new Page<>(page, 100)
                         , new QueryWrapper<FundRecord>().lambda().eq(FundRecord::getStatus, FundRecordStatus.PROCESS))
                 .getRecords();
-
         records.forEach(fundRecord -> {
-            //收益记录状态改变
-            List<FundIncomeRecord> fundIncomeRecords = fundIncomeRecordService.list(new QueryWrapper<FundIncomeRecord>().lambda()
-                    .eq(FundIncomeRecord::getFundId, fundRecord.getId())
-                    .eq(FundIncomeRecord::getStatus, FundIncomeStatus.calculated));
-            fundIncomeRecords.forEach(fundIncomeRecord -> {
-                LocalDateTime fundIncomeRecordCreateTime = fundIncomeRecord.getCreateTime();
-                if(fundIncomeRecordCreateTime.until(now, ChronoUnit.DAYS) >= FundCycle.interestAuditCycle){
-                    fundIncomeRecord.setStatus(FundIncomeStatus.wait_audit);
-                    fundIncomeRecordService.updateById(fundIncomeRecord);
-                }
-                //待发放收益
-                fundRecord.setWaitIncomeAmount(fundRecord.getIncomeAmount().add(fundIncomeRecord.getInterestAmount()));
-            });
-
-            LocalDateTime createTime = fundRecord.getCreateTime();
-            //四天后开始计息
-            if(createTime.until(now, ChronoUnit.DAYS) >= FundCycle.interestCalculationCycle){
-                BigDecimal dailyIncome = fundRecordService.dailyIncome(fundRecord.getHoldAmount(), fundRecord.getRate());
-                //收益记录
-                FundIncomeRecord incomeRecord = FundIncomeRecord.builder()
-                        .uid(fundRecord.getUid())
-                        .fundId(fundRecord.getId())
-                        .productId(fundRecord.getProductId())
-                        .productName(fundRecord.getProductName())
-                        .coin(fundRecord.getCoin())
-                        .rate(fundRecord.getRate())
-                        .holdAmount(fundRecord.getHoldAmount())
-                        .interestAmount(dailyIncome)
-                        .status(FundIncomeStatus.calculated)
-                        .createTime(now)
-                        .build();
-                fundIncomeRecordService.save(incomeRecord);
-
-                //累计收益
-                fundRecord.setCumulativeIncomeAmount(fundRecord.getCumulativeIncomeAmount().add(dailyIncome));
-
+            RLock lock = redissonClient.getLock(RedisLockConstants.FUND_UPDATE_LOCK + fundRecord.getId());
+            try {
+                lock.lock();
+                calculateIncome(fundRecord,now);
+            } finally {
+                lock.unlock();
             }
-            fundRecordService.updateById(fundRecord);
+
         });
+    }
+
+    public void calculateIncome(FundRecord fundRecord,LocalDateTime now){
+        //收益记录状态改变
+        List<FundIncomeRecord> fundIncomeRecords = fundIncomeRecordService.list(new QueryWrapper<FundIncomeRecord>().lambda()
+                .eq(FundIncomeRecord::getFundId, fundRecord.getId())
+                .eq(FundIncomeRecord::getStatus, FundIncomeStatus.calculated));
+        fundIncomeRecords.forEach(fundIncomeRecord -> {
+            LocalDateTime fundIncomeRecordCreateTime = fundIncomeRecord.getCreateTime();
+            if(fundIncomeRecordCreateTime.until(now, ChronoUnit.DAYS) >= FundCycle.interestAuditCycle){
+                fundIncomeRecord.setStatus(FundIncomeStatus.wait_audit);
+                fundIncomeRecordService.updateById(fundIncomeRecord);
+            }
+            //待发放收益
+            fundRecord.setWaitIncomeAmount(fundRecord.getIncomeAmount().add(fundIncomeRecord.getInterestAmount()));
+        });
+
+        LocalDateTime createTime = fundRecord.getCreateTime();
+        //四天后开始计息
+        if(createTime.until(now, ChronoUnit.DAYS) >= FundCycle.interestCalculationCycle){
+            BigDecimal dailyIncome = fundRecordService.dailyIncome(fundRecord.getHoldAmount(), fundRecord.getRate());
+            //收益记录
+            FundIncomeRecord incomeRecord = FundIncomeRecord.builder()
+                    .uid(fundRecord.getUid())
+                    .fundId(fundRecord.getId())
+                    .productId(fundRecord.getProductId())
+                    .productName(fundRecord.getProductName())
+                    .coin(fundRecord.getCoin())
+                    .rate(fundRecord.getRate())
+                    .holdAmount(fundRecord.getHoldAmount())
+                    .interestAmount(dailyIncome)
+                    .status(FundIncomeStatus.calculated)
+                    .createTime(now)
+                    .build();
+            fundIncomeRecordService.save(incomeRecord);
+
+            //累计收益
+            fundRecord.setCumulativeIncomeAmount(fundRecord.getCumulativeIncomeAmount().add(dailyIncome));
+
+        }
+        fundRecordService.updateById(fundRecord);
     }
 
     public Long incr(String key, long liveTime) {
