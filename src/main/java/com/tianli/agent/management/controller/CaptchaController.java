bf6a1d1cc177bc41ae555ba4c61216d746781dfb
ziyuan <1149788726@qq.com>
2022-09-13 18:00:01 +0800

    [fix] 验证码fix

--- src/main/java/com/tianli/agent/management/controller/CaptchaController.java
+++ src/main/java/com/tianli/agent/management/controller/CaptchaController.java
@@ -1,7 +1,6 @@
 package com.tianli.agent.management.controller;
 
 import cn.hutool.core.util.IdUtil;
-import com.google.code.kaptcha.Producer;
 import com.tianli.agent.management.vo.CaptchaImageVO;
 import com.tianli.common.RedisConstants;
 import com.tianli.exception.Result;
@@ -9,14 +8,11 @@ import com.wf.captcha.SpecCaptcha;
 import com.wf.captcha.base.Captcha;
 import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
-import org.springframework.util.FastByteArrayOutputStream;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
@@ -26,8 +22,6 @@ import java.util.concurrent.TimeUnit;
 @RestController
 @RequestMapping("/agent/management")
 public class CaptchaController {
-    @Resource(name = "captchaProducer")
-    private Producer captchaProducer;
 
     @Resource
     private RedissonClient redissonClient;
@@ -41,22 +35,14 @@ public class CaptchaController {
         String uuid = IdUtil.simpleUUID();
         String verifyKey = RedisConstants.CAPTCHA_CODE_KEY + uuid;
 
-        String capStr, code;
-        BufferedImage image;
+        String code;
         Captcha captcha = new SpecCaptcha(115, 42);
 
         // 生成验证码
-        capStr = code = captcha.text();
-        image = captchaProducer.createImage(capStr);
+        code = captcha.text();
         RBucket<Object> bucket = redissonClient.getBucket(verifyKey);
         bucket.set(code, 5L, TimeUnit.MINUTES);
-        // 转换流信息写出
-        FastByteArrayOutputStream os = new FastByteArrayOutputStream();
-        try {
-            ImageIO.write(image, "jpg", os);
-        } catch (IOException e) {
-            return Result.fail(e.getMessage());
-        }
+
         CaptchaImageVO captchaImageVO = CaptchaImageVO.builder()
                 .uuid(uuid)
                 .img(captcha.toBase64())
