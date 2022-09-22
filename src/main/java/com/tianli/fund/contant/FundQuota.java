28980c060aa0f7addd8194fa7882675cba07e108
ziyuan <1149788726@qq.com>
2022-09-22 14:10:46 +0800

    [feat] 基金申购数额修改

--- src/main/java/com/tianli/fund/contant/FundQuota.java
+++ src/main/java/com/tianli/fund/contant/FundQuota.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public interface FundQuota {
        // 5000/10000/15000/20000
-    List<String> quotas = List.of("5000", "10000", "15000", "20000","5", "10", "15", "20");
+    List<String> quotas = List.of("5000", "10000", "15000", "20000");
 
 }
