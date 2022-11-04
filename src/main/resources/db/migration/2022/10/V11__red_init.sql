bdc5bf2317768cba9a7e1bb0cce2fd6c53495166
ziyuan <1149788726@qq.com>
2022-11-04 17:18:27 +0800

    [feat] 红包建表添加txid唯一索引

--- src/main/resources/db/migration/2022/10/V11__red_init.sql
+++ src/main/resources/db/migration/2022/10/V11__red_init.sql
@@ -20,6 +20,7 @@ CREATE TABLE `red_envelope`
     `status`       varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '红包状态',
     `txid`         varchar(255)                                                          DEFAULT NULL COMMENT '交易hash',
     PRIMARY KEY (`id`),
+    UNIQUE KEY `unique_hash` (`txid`) COMMENT 'hash唯一主键',
     KEY `expire_index` (`status`, `create_time` DESC),
     KEY `uid_index` (`uid`)
 ) ENGINE = InnoDB
