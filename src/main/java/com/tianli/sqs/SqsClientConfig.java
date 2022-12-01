b32c3e2c7068c54bb32bef6d2a5fe41b4a46bca4
ziyuan <1149788726@qq.com>
2022-12-01 17:21:32 +0800

    后台改造：[feat] 手动添加sqs环境变量

--- src/main/java/com/tianli/sqs/SqsClientConfig.java
+++ src/main/java/com/tianli/sqs/SqsClientConfig.java
@@ -1,17 +1,14 @@
 package com.tianli.sqs;
 
-import cn.hutool.json.JSONUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
-import software.amazon.awssdk.services.sqs.model.QueueAttributeName;
-import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import java.util.HashMap;
 
 /**
  * @author chenb
@@ -22,11 +19,27 @@ import java.util.HashMap;
 public class SqsClientConfig {
 
     private SqsClient sqsClient;
+    @Value("${WS_ACCESS_KEY_ID}")
+    private String WS_ACCESS_KEY_ID;
+    @Value("${AWS_SECRET_ACCESS_KEY}")
+    private String AWS_SECRET_ACCESS_KEY;
 
     @PostConstruct
     public void init() {
         sqsClient = SqsClient.builder().credentialsProvider(EnvironmentVariableCredentialsProvider.create())
-                .region(Region.AP_NORTHEAST_1).build();
+                .region(Region.AP_NORTHEAST_1)
+                .credentialsProvider(() -> new AwsCredentials() {
+                    @Override
+                    public String accessKeyId() {
+                        return WS_ACCESS_KEY_ID;
+                    }
+
+                    @Override
+                    public String secretAccessKey() {
+                        return AWS_SECRET_ACCESS_KEY;
+                    }
+                })
+                .build();
     }
 
     @PreDestroy
