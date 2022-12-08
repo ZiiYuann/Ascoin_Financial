0208d0df22da1558126e4373bff609f5dd9866b2
ziyuan <1149788726@qq.com>
2022-12-08 18:22:17 +0800

    OPENAPI：[feat] 更换位置

--- src/main/java/com/tianli/management/controller/ManageUserController.java
+++ src/main/java/com/tianli/management/controller/ManageUserController.java
@@ -1,14 +1,18 @@
 package com.tianli.management.controller;
 
-import com.tianli.account.service.AccountBalanceService;
 import com.tianli.address.mapper.Address;
 import com.tianli.common.PageQuery;
 import com.tianli.exception.Result;
 import com.tianli.financial.service.FinancialService;
-import com.tianli.management.query.UidsQuery;
+import com.tianli.openapi.query.OpenapiAccountQuery;
+import com.tianli.openapi.service.OpenApiService;
+import com.tianli.openapi.vo.StatisticsData;
 import com.tianli.sso.permission.AdminPrivilege;
 import com.tianli.sso.permission.Privilege;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -48,8 +52,26 @@ public class ManageUserController {
         return Result.success().setData(financialService.userAmountDetailsVO(uid));
     }
 
+    /**
+     * 账户信息
+     */
+    @GetMapping("/account")
+    @AdminPrivilege(and = Privilege.理财管理)
+    public Result statisticsData(OpenapiAccountQuery query) {
+        return Result.success(openApiService.accountData(query));
+    }
+
+    /**
+     * 账户信息
+     */
+    @GetMapping("/account/sub")
+    @AdminPrivilege(and = Privilege.理财管理)
+    public Result statisticsData(Long chatId, PageQuery<StatisticsData> pageQuery) {
+        return Result.success(openApiService.accountSubData(chatId, pageQuery));
+    }
+
     @Resource
     private FinancialService financialService;
     @Resource
-    private AccountBalanceService accountBalanceService;
+    private OpenApiService openApiService;
 }
--- src/main/java/com/tianli/openapi/controller/OpenApiController.java
+++ src/main/java/com/tianli/openapi/controller/OpenApiController.java
@@ -3,16 +3,11 @@ package com.tianli.openapi.controller;
 import com.tianli.account.service.AccountBalanceService;
 import com.tianli.account.service.impl.AccountBalanceServiceImpl;
 import com.tianli.charge.service.ChargeService;
-import com.tianli.common.PageQuery;
 import com.tianli.exception.ErrorCodeEnum;
 import com.tianli.exception.Result;
 import com.tianli.management.query.UidsQuery;
-import com.tianli.openapi.query.OpenapiAccountQuery;
 import com.tianli.openapi.query.OpenapiOperationQuery;
 import com.tianli.openapi.service.OpenApiService;
-import com.tianli.openapi.vo.StatisticsData;
-import com.tianli.sso.permission.AdminPrivilege;
-import com.tianli.sso.permission.Privilege;
 import com.tianli.tool.crypto.Crypto;
 import org.bouncycastle.crypto.util.DigestFactory;
 import org.springframework.web.bind.annotation.*;
@@ -113,27 +108,15 @@ public class OpenApiController {
     }
 
     /**
-     * 账户信息
-     */
-    @GetMapping("/account")
-    public Result statisticsData(OpenapiAccountQuery query) {
-        return Result.success(openApiService.accountData(query));
-    }
-
-    /**
-     * 账户信息
-     */
-    @GetMapping("/account/sub")
-    public Result statisticsData(Long chatId, PageQuery<StatisticsData> pageQuery) {
-        return Result.success(openApiService.accountSubData(chatId, pageQuery));
-    }
-
-
-    /**
      * 用户资产
      */
     @GetMapping("/assets/{uid}")
-    public Result assets(@PathVariable Long uid) {
+    public Result assets(@PathVariable Long uid,
+                         @RequestHeader("sign") String sign,
+                         @RequestHeader("timestamp") String timestamp) {
+        if (!Crypto.hmacToString(DigestFactory.createSHA256(), "vUfV1n#JdyG^oKCb", timestamp).equals(sign)) {
+            throw ErrorCodeEnum.SIGN_ERROR.generalException();
+        }
         return Result.success().setData(accountBalanceService.getUserAssetsVO(uid));
     }
 
@@ -141,7 +124,12 @@ public class OpenApiController {
      * 用户资产
      */
     @PostMapping("/assets/uids")
-    public Result assetsUids(@RequestBody UidsQuery query) {
+    public Result assetsUids(@RequestBody UidsQuery query,
+                             @RequestHeader("sign") String sign,
+                             @RequestHeader("timestamp") String timestamp) {
+        if (!Crypto.hmacToString(DigestFactory.createSHA256(), "vUfV1n#JdyG^oKCb", timestamp).equals(sign)) {
+            throw ErrorCodeEnum.SIGN_ERROR.generalException();
+        }
         return Result.success().setData(accountBalanceService.getUserAssetsVO(query.getUids()));
     }
 
@@ -149,7 +137,12 @@ public class OpenApiController {
      * 用户资产
      */
     @PostMapping("/assets/map")
-    public Result assetsMap(@RequestBody UidsQuery query) {
+    public Result assetsMap(@RequestBody UidsQuery query,
+                            @RequestHeader("sign") String sign,
+                            @RequestHeader("timestamp") String timestamp) {
+        if (!Crypto.hmacToString(DigestFactory.createSHA256(), "vUfV1n#JdyG^oKCb", timestamp).equals(sign)) {
+            throw ErrorCodeEnum.SIGN_ERROR.generalException();
+        }
         return Result.success().setData(accountBalanceService.getUserAssetsVOMap(query.getUids()));
     }
 
