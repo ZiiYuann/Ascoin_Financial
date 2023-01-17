151595e52f7a0cce9c8e830386cfeab6b733efb3
ziyuan <1149788726@qq.com>
2023-01-17 16:26:04 +0800

    HOTFIX20230117:[fix] 异常账户数据修复

---
+++ src/main/resources/db/migration/2023/01/V25__account_ifx_20230117.sql
@@ -0,0 +1,5 @@
+
+
+UPDATE account_balance set balance = 0.01140849, remain = 0.00029988 ,freeze = 0.01110861 where id = 1755102503577366119;
+
+DELETE FROM account_balance_operation_log where id = 1755252062127733119;
