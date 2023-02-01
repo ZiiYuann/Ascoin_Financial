b99f9ec56284de2c6bac1a6f0f27b19b8e8b76dd
ziyuan <1149788726@qq.com>
2023-02-01 11:58:43 +0800

    空投：[feat] 空投add

--- src/main/java/com/tianli/charge/enums/ChargeGroup.java
+++ src/main/java/com/tianli/charge/enums/ChargeGroup.java
@@ -15,7 +15,7 @@ public enum ChargeGroup {
     receive(List.of(ChargeType.recharge, ChargeType.income, ChargeType.redeem, ChargeType.settle
             , ChargeType.borrow, ChargeType.release, ChargeType.agent_fund_sale, ChargeType.fund_redeem
             , ChargeType.fund_interest, ChargeType.red_back, ChargeType.red_get, ChargeType.transaction_reward
-            , ChargeType.transfer_increase,ChargeType.return_gas)),
+            , ChargeType.transfer_increase,ChargeType.return_gas,ChargeType.airdrop)),
     // 支出
     pay(List.of(ChargeType.withdraw, ChargeType.purchase, ChargeType.transfer, ChargeType.repay
             , ChargeType.pledge, ChargeType.fund_purchase, ChargeType.agent_fund_redeem, ChargeType.agent_fund_interest
