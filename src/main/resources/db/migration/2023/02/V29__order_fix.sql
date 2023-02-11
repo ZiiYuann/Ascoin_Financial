bcb97a5e81feaa45d85333cac895afd1bd41220d
ziyuan <1149788726@qq.com>
2023-02-11 11:01:32 +0800

    fix

---
+++ src/main/resources/db/migration/2023/02/V29__order_fix.sql
@@ -0,0 +1,3 @@
+
+UPDATE `order_charge_info` set txid = '0x9aaf9ebc192eead6d142290d334873a780731c8d7249c83c9699c3cb112cf8aa'
+ and to_address='0xb531f475f0ea80630125878e2223fb0b56d29edc' and create_time = '2023-02-02 08:20:03' where id = 1756626942356714806;
