0bfd4668fef787a3de9c05dab9fc0348021c4375
ziyuan <1149788726@qq.com>
2023-02-16 10:55:31 +0800

    站外红包:[feat] 管理端新增controller

---
+++ src/main/java/com/tianli/management/controller/ManagerRedEnvelopeController.java
@@ -0,0 +1,14 @@
+package com.tianli.management.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chenb
+ * @apiNote
+ * @since 2023-02-16
+ **/
+@RestController
+@RequestMapping("/management/red")
+public class ManagerRedEnvelopeController {
+}
