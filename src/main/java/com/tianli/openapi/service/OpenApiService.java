6eeef2953f9c8cec8fd3e766168311b63d3e77f8
ziyuan <1149788726@qq.com>
2023-02-21 13:40:24 +0800

    部分接口添加uid

--- src/main/java/com/tianli/openapi/service/OpenApiService.java
+++ src/main/java/com/tianli/openapi/service/OpenApiService.java
@@ -283,6 +283,7 @@ public class OpenApiService {
             data.setSubBalance(allUserAssetsVO.getBalanceAmount());
             data.setSubPurchaseAmount(allUserAssetsVO.getPurchaseAmount());
             data.setSubRedeemAmount(subRedeemAmount);
+            data.setUid(uid);
         }
 
         return data;
