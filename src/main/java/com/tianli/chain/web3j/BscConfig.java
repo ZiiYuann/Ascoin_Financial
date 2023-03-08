6cfb229b8b0ec49b58a3d0bd34696a58806f9283
chenbin <1149788726@qq.com>
2023-03-08 10:43:30 +0800

    bsc更换节点

--- src/main/java/com/tianli/chain/web3j/BscConfig.java
+++ src/main/java/com/tianli/chain/web3j/BscConfig.java
@@ -12,18 +12,30 @@ import javax.annotation.Resource;
 @Configuration
 public class BscConfig {
 
-    private static final String BINANCE_SMART_CHAIN_MAIN_NET = "http://18.183.222.219:8545/C676M3pn5kaSdSq";
+    private static final String BINANCE_SMART_CHAIN_MAIN_NET = "http://52.192.186.24:8545/C676M3pn5kaSdSq";
+    private static final String BINANCE_SMART_CHAIN_MAIN_NET1 = "https://bsc-dataseed1.ninicoin.io";
 
     @Resource
     private ConfigService configService;
 
     @Bean
-    public JsonRpc2_0Web3j bscWeb3j(){
+    public JsonRpc2_0Web3j bscWeb3j() {
         String bsc_url;
         try {
             bsc_url = configService.getOrDefault(ConfigConstants.BSC_CHAIN_URL, BINANCE_SMART_CHAIN_MAIN_NET);
         } catch (Exception e) {
             bsc_url = BINANCE_SMART_CHAIN_MAIN_NET;
+        }
+        return new JsonRpc2_0Web3j(new HttpService(bsc_url));
+    }
+
+    @Bean
+    public JsonRpc2_0Web3j bscWeb3j1() {
+        String bsc_url;
+        try {
+            bsc_url = configService.getOrDefault(ConfigConstants.BSC_CHAIN_URL, BINANCE_SMART_CHAIN_MAIN_NET1);
+        } catch (Exception e) {
+            bsc_url = BINANCE_SMART_CHAIN_MAIN_NET1;
         }
         return new JsonRpc2_0Web3j(new HttpService(bsc_url));
     }
