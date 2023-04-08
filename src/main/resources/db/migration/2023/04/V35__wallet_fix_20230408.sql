bb29756e786a82c64e1dd6db0db351027f1b9eb0
chenbin <1149788726@qq.com>
2023-04-08 13:34:37 +0800

    修复归集历史数据

---
+++ src/main/resources/db/migration/2023/04/V35__wallet_fix_20230408.sql
@@ -0,0 +1,52 @@
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1609894775327887363, '0x4c7ad65c3690ab9cfb357ed7ea7d9c8a5667e97654617113c81b2272dd50c2b5', 22.69459800, NULL, '0x6943c0a2c40a38fa6cf72d16bc3dcf7bb06f62b4', 'bep20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1609894775327887364, '0x4c7ad65c3690ab9cfb357ed7ea7d9c8a5667e97654617113c81b2272dd50c2b5', 90.00000000, NULL, '0x6943c0a2c40a38fa6cf72d16bc3dcf7bb06f62b4', 'bep20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1630053641613910020, '9b566e87b97b9b4fcbd005a1d7dcb63048eb1d05c7f10ccb48b2885690666123', 27.00000000, NULL, 'TGntEwCeZ6gLDR7XisTEJ6igxEHx9KYxue', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1622994769819926532, 'bb29b5ded47c623a9e63bf4eddc27ad5aeabf8a8df0b00fb919f04a80cf8eb88', 25.00000000, NULL, 'TYHbc9o7ZhDz5qd94ixB3igNTiRT36Xytu', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1629051749268504577, 'c24babaf82bd35363079df7f0e26f22c9ff102c0805c49732bbd7e488af16a60', 6950.00000000, NULL, 'TKJP45EFgA4RMgDKNPEEFYrmwYbeJCteaA', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1629051749268504578, 'c24babaf82bd35363079df7f0e26f22c9ff102c0805c49732bbd7e488af16a60', 6950.00000000, NULL, 'TKJP45EFgA4RMgDKNPEEFYrmwYbeJCteaA', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1629101949395079176, 'ec77c75b79bc288f37986c05f00c37380eba47af42c808f226decc7a9b5e26d9', 1000.00000000, NULL, 'TCySJYT5LKXTD3QdEM4vRiDdCxWmcDuyZt', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1629101949395079174, 'ec77c75b79bc288f37986c05f00c37380eba47af42c808f226decc7a9b5e26d9', 63.00000000, NULL, 'TMLeXBuNei84H1ePisofuKRAyzagVcwt8Q', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1629101949395079177, 'ec77c75b79bc288f37986c05f00c37380eba47af42c808f226decc7a9b5e26d9', 63.00000000, NULL, 'TMLeXBuNei84H1ePisofuKRAyzagVcwt8Q', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1629101949395079175, 'ec77c75b79bc288f37986c05f00c37380eba47af42c808f226decc7a9b5e26d9', 95.00000000, NULL, 'TRkN9v32tVZF3z82zfY6vTtCmUbT1iEbcJ', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1606864084246167564, 'fef0285f05c6c7dc09d5a2f7ce6ba25c6092402c5ffc5e42664af579f4bf6f62', 10.00000000, NULL, 'TMdsMERCFCTDpwcbUKKG5dhxUA7pqaEBqw', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1606864084246167560, 'fef0285f05c6c7dc09d5a2f7ce6ba25c6092402c5ffc5e42664af579f4bf6f62', 10.00000000, NULL, 'TMLeXBuNei84H1ePisofuKRAyzagVcwt8Q', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1606864084246167562, 'fef0285f05c6c7dc09d5a2f7ce6ba25c6092402c5ffc5e42664af579f4bf6f62', 20.00000000, NULL, 'TMLeXBuNei84H1ePisofuKRAyzagVcwt8Q', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1606864084250361859, 'fef0285f05c6c7dc09d5a2f7ce6ba25c6092402c5ffc5e42664af579f4bf6f62', 5.00000000, NULL, 'TMLeXBuNei84H1ePisofuKRAyzagVcwt8Q', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1606864084250361860, 'fef0285f05c6c7dc09d5a2f7ce6ba25c6092402c5ffc5e42664af579f4bf6f62', 100.00000000, NULL, 'TYp2oPsXNw4JyGCzhCwKJ1vBd39ikdFrbw', 'trc20');
+-- INSERT INTO`wallet_imputation_log_appendix` (`id`, `txid`, `amount`, `to_address`, `from_address`, `network`) VALUES (1606864084250361861, 'fef0285f05c6c7dc09d5a2f7ce6ba25c6092402c5ffc5e42664af579f4bf6f62', 0.00000000, NULL, 'TYp2oPsXNw4JyGCzhCwKJ1vBd39ikdFrbw', 'trc20');
+
+
+DELETE
+FROM wallet_imputation_log_appendix
+WHERE id in (
+             1609894775327887363,
+             1609894775327887364,
+             1630053641613910020,
+             1622994769819926532,
+             1629051749268504577,
+             1629051749268504578,
+             1629101949395079176,
+             1629101949395079174,
+             1629101949395079177,
+             1629101949395079175,
+             1606864084246167564,
+             1606864084246167560,
+             1606864084246167562,
+             1606864084250361859,
+             1606864084250361860,
+             1606864084250361861
+    );
+
+
+UPDATE wallet_imputation_log log
+
+    INNER JOIN (SELECT sum(amount) as amount ,txid from wallet_imputation_log_appendix  WHERE txid in (
+    '0x4c7ad65c3690ab9cfb357ed7ea7d9c8a5667e97654617113c81b2272dd50c2b5',
+    '9b566e87b97b9b4fcbd005a1d7dcb63048eb1d05c7f10ccb48b2885690666123',
+    'bb29b5ded47c623a9e63bf4eddc27ad5aeabf8a8df0b00fb919f04a80cf8eb88',
+    'c24babaf82bd35363079df7f0e26f22c9ff102c0805c49732bbd7e488af16a60',
+    'ec77c75b79bc288f37986c05f00c37380eba47af42c808f226decc7a9b5e26d9',
+    'fef0285f05c6c7dc09d5a2f7ce6ba25c6092402c5ffc5e42664af579f4bf6f62'
+    ) GROUP BY txid ) temp
+
+on log.txid = temp.txid set log.amount = temp.amount;
